af4da71ddc8bc04dfa859d9429e05eada8045666
aradhya-tiwari <aradhya.prad@gmail.com>
2026-03-22 16:22:53 +0530

    students added

---
+++ js/aradhya_tiwari_student_analyzer.js
@@ -0,0 +1,38 @@
+const students = [
+  {
+    name: "Lalit",
+    marks: [
+      { subject: "Math", score: 78 },
+      { subject: "English", score: 82 },
+      { subject: "Science", score: 74 },
+      { subject: "History", score: 69 },
+      { subject: "Computer", score: 88 }
+    ],
+    attendance: 82
+  },
+  {
+    name: "Rahul",
+    marks: [
+      { subject: "Math", score: 90 },
+      { subject: "English", score: 85 },
+      { subject: "Science", score: 80 },
+      { subject: "History", score: 76 },
+      { subject: "Computer", score: 92 }
+    ],
+    attendance: 91
+  },
+
+  {
+    name: "Aradhya",
+    marks: [
+      { subject: "Math", score: 86 },
+      { subject: "English", score: 85 },
+      { subject: "Science", score: 94 },
+      { subject: "History", score: 82 },
+      { subject: "Computer", score: 92 }
+    ],
+    attendance: 81
+  }
+];
+
+console.log(students)
