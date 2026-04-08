5e847e598cbc8c95d4e0dbae7bbf575845e598de
aradhya-tiwari <aradhya.prad@gmail.com>
2026-04-08 17:35:47 +0530

    feat: all array programs

---
+++ java/array/ArrayAverage.java
@@ -0,0 +1,26 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArrayAverage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        int sum = 0;
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+            sum += arr[i];
+        }
+
+        double average = (double) sum / n;
+        System.out.println("Average of array elements: " + average);
+
+        scanner.close();
+    }
+}
