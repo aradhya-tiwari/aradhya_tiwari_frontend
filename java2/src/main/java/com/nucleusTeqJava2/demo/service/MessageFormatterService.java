3fbe5033b4a99a0348f50b9bc3f783960f2a6d2c
aradhya-tiwari <aradhya.prad@gmail.com>
2026-04-14 10:11:10 +0530

    feat: message formatter service

---
+++ java2/src/main/java/com/nucleusTeqJava2/demo/service/MessageFormatterService.java
@@ -0,0 +1,28 @@
+package com.nucleusTeqJava2.demo.service;
+
+import com.nucleusTeqJava2.demo.component.ShortMessageFormatter;
+import com.nucleusTeqJava2.demo.component.LongMessageFormatter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageFormatterService {
+    private ShortMessageFormatter shortMessageFormatter;
+    private LongMessageFormatter longMessageFormatter;
+
+    public MessageFormatterService(ShortMessageFormatter shortMessageFormatter, 
+                                  LongMessageFormatter longMessageFormatter) {
+        this.shortMessageFormatter = shortMessageFormatter;
+        this.longMessageFormatter = longMessageFormatter;
+    }
+
+    public String formatMessage(String content, String type) {
+        switch (type.toLowerCase()) {
+            case "short":
+                return shortMessageFormatter.format(content);
+            case "long":
+                return longMessageFormatter.format(content);
+            default:
+                throw new IllegalArgumentException("Type must be SHORT or LONG");
+        }
+    }
+}
