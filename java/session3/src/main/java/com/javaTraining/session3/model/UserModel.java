5ec1399dae6662c08679192af9dffa2f8ad9c446
aradhya-tiwari <aradhya.prad@gmail.com>
2026-04-16 18:25:32 +0530

    feat:user model

---
+++ java/session3/src/main/java/com/javaTraining/session3/model/UserModel.java
@@ -0,0 +1,50 @@
+package com.javaTraining.session3.model;
+
+public class UserModel {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String role;
+
+    public UserModel() {
+    }
+
+    public UserModel(Long id, String name, Integer age, String role) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
