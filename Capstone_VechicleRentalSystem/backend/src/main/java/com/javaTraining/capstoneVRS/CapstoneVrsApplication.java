4eebea005d729cb6b993734285c37ba5ba9205fe
aradhya-tiwari <aradhya.prad@gmail.com>
2026-04-21 09:15:03 +0530

    chore:backend initialised

---
+++ Capstone_VechicleRentalSystem/backend/src/main/java/com/javaTraining/capstoneVRS/CapstoneVrsApplication.java
@@ -0,0 +1,13 @@
+package com.javaTraining.capstoneVRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneVrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneVrsApplication.class, args);
+	}
+
+}
