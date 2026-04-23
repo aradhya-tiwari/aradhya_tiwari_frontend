a2f141d4127363feaedcbca3f12331c1fe4e14f2
aradhya-tiwari <aradhya.prad@gmail.com>
2026-04-23 23:38:34 +0530

    timezone change in springboot try1

--- Capstone_VechicleRentalSystem/backend/src/main/java/com/javaTraining/capstoneVRS/CapstoneVrsApplication.java
+++ Capstone_VechicleRentalSystem/backend/src/main/java/com/javaTraining/capstoneVRS/CapstoneVrsApplication.java
@@ -1,10 +1,16 @@
 package com.javaTraining.capstoneVRS;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CapstoneVrsApplication {
+
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneVrsApplication.class, args);
