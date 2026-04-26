60ac35d1f5b4ac80612545439006d159ca2c6312
aradhya-tiwari <aradhya.prad@gmail.com>
2026-04-26 18:26:28 +0530

    Time zone change in Main class

--- Capstone_VechicleRentalSystem/backend/src/main/java/com/javaTraining/capstoneVRS/CapstoneVrsApplication.java
+++ Capstone_VechicleRentalSystem/backend/src/main/java/com/javaTraining/capstoneVRS/CapstoneVrsApplication.java
@@ -1,5 +1,6 @@
 package com.javaTraining.capstoneVRS;
 
+import java.time.ZoneId;
 import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
@@ -8,12 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CapstoneVrsApplication {
 
-	public void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
-	}
-
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+		ZoneId currentZone = ZoneId.systemDefault();
+		System.out.println("Backend Started...");
+		System.out.println(currentZone);
 		SpringApplication.run(CapstoneVrsApplication.class, args);
+
 	}
 
 }
