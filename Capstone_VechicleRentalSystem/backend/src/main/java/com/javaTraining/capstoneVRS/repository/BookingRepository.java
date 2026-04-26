75a2521b9f682a7cc44bc4320edace3d3d7156a5
aradhya-tiwari <aradhya.prad@gmail.com>
2026-04-26 22:43:32 +0530

    feat: booking repository with query to chek booking available or not

---
+++ Capstone_VechicleRentalSystem/backend/src/main/java/com/javaTraining/capstoneVRS/repository/BookingRepository.java
@@ -0,0 +1,22 @@
+package com.javaTraining.capstoneVRS.repository;
+
+import com.javaTraining.capstoneVRS.entity.Booking;
+import com.javaTraining.capstoneVRS.entity.BookingStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    // check if in the vehicles entity has vehicleId (as we pass it) AND status enum
+    // BookingStatus.
+    // Start date and End date should be in between the provided date
+    boolean existsByVehicleVehicleIdAndStatusInAndStartDateLessThanEqualAndEndDateGreaterThanEqual(
+            Long vehicleId,
+            Collection<BookingStatus> statuses,
+            LocalDate endDate,
+            LocalDate startDate);
+
+    List<Booking> findByUserUserIdOrderByCreatedAtDesc(Long userId);
+}
