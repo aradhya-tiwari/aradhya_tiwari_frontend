2c609df548a69fecc0f93e12403379c21936d61e
aradhya-tiwari <aradhya.prad@gmail.com>
2026-04-29 10:23:53 +0530

    fix: system.out.print removed

--- Capstone_VechicleRentalSystem/backend/src/main/java/com/javaTraining/capstoneVRS/CapstoneVrsApplication.java
+++ Capstone_VechicleRentalSystem/backend/src/main/java/com/javaTraining/capstoneVRS/CapstoneVrsApplication.java
@@ -12,8 +12,6 @@ public class CapstoneVrsApplication {
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		ZoneId currentZone = ZoneId.systemDefault();
-		System.out.println("Backend Started...");
-		System.out.println(currentZone);
 		SpringApplication.run(CapstoneVrsApplication.class, args);
 
 	}
