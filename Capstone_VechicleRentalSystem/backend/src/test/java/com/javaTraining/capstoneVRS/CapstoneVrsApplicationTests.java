820ba35f2459b9fe850239e1543065b061a753e1
aradhya-tiwari <aradhya.prad@gmail.com>
2026-05-05 17:02:41 +0530

    test: SpringBootTest added to main

--- Capstone_VechicleRentalSystem/backend/src/test/java/com/javaTraining/capstoneVRS/CapstoneVrsApplicationTests.java
+++ Capstone_VechicleRentalSystem/backend/src/test/java/com/javaTraining/capstoneVRS/CapstoneVrsApplicationTests.java
@@ -1,12 +1,14 @@
 package com.javaTraining.capstoneVRS;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
 class CapstoneVrsApplicationTests {
 
 	@Test
-	void testHarnessIsAvailable() {
+	void contextLoads() {
 		assertTrue(true);
 	}
 
